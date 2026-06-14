546ca2452e863556d3fda79382d76da0695f236c
Camila Rodrigues Moura <61846704+Camila10438@users.noreply.github.com>
2026-06-14 10:44:43 -0300

    Add script

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
+amount.addEventListener("input", () => {
+    const hasCaracteresRegex = /\D+/g 
+    amount.value = amount.value.replace(hasCaracteresRegex, "")
+})
+
+form.submit = function(event) {
+    event.preventDefault()
+
+    switch(currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+function convertCurrency(amount, price, symbol){
+    try{
+        footer.classList.add("show-result")
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+        let total = amount * price
+        total = formatCurrencyBRL(total).replace("R$", "")
+        result.textContent = `${total} Reais`
+    }
+    catch(error){
+        footer.classList.remove("show-result")
+        console.log(error)
+        alert("Não foi possível converter. Tente novamente!")
+    }
+}
+
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR", {
+        style: currency,
+        currency: BRL
+    })
+}
